f711e76b452bb84c34bf641ee54502ac0b889f59
Jayguar <128915852+Jayguar@users.noreply.github.com>
2026-01-03 18:47:18 +0000

    Update background.js

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (changeInfo.status === "complete" && tab.url && tab.url.includes("youtube.com/shorts")) {
     chrome.tabs.update(tabId, {url: "https://www.youtube.com/"});
   }
-  if (changeInfo.status === "complete" && tab.url && tab.url.includes("instagram.com/reels")) {
+  if (changeInfo.status === "complete" && tab.url && (tab.url.includes("instagram.com/reels") || tab.url.includes("instagram.com/p"))) {
     chrome.tabs.update(tabId, {url: "https://www.instagram.com/"});
   }
 });
